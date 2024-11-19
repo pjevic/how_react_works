95406b6e415b66b17cf6d34ae2d9ac433d741652
Milan Pjevic <milanpjevic@icloud.com>
2024-11-19 11:08:44 +0100

    Diffing - Reseting State With The Key Prop

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent key={content.at(activeTab).summary} item={content.at(activeTab)} />
       ) : (
         <DifferentContent />
       )}
